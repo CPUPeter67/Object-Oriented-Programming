4a3759733108ade6cd38889aea395887f0b8f4d9
CPUPetet <c.nwodo@abujaprep.com>
2026-06-19 16:52:10 +0100

    dog_breed.py

---
+++ Dogbreed.py
@@ -0,0 +1,12 @@
+class DogBreed:
+    def __init__(self, name, weight, breed, age):
+        self.name = "Rox"
+        self.weight = "30kg"
+        self.breed = "Saint Bernard"
+        self.age = "5"
+
+        def display_info(self):
+            print(f"Name: {self.name}")
+            print(f"Weight: {self.weight}")
+            print(f"Breed: {self.breed}")
+            print(f"Age: {self.breed}")
